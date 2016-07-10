593e85e5ca28111bc39c9915fcdc2ed59d1c3c8e
mohammad suliman <mohmad.s93@gmail.com>
2016-07-11 01:55:46 +0300

    add support for express versions ov VS

---
+++ addon/appModules/wdexpress.py
@@ -0,0 +1,4 @@
+# appModule for visual studio
+#author: mohammad suliman (mohmad.s93@gmail.com)
+#this app module is for visual studio express, we use the same exact module of the community edition (devenv.py)
+from devenv import *
