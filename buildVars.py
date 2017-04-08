a004570a671bb08bcfd137a0d42f442fad4e4ab1
mohammad suliman <mohmad.s93@gmail.com>
2017-04-08 14:37:00 +0300

    update buildVars

--- buildVars.py
+++ buildVars.py
@@ -34,8 +34,7 @@ import os.path
 
 # Define the python files that are the sources of your add-on.
 # You can use glob expressions here, they will be expanded.
-pythonSources = []
-
+pythonSources = [os.path.join("addon", "appModules", "*.py")]
 # Files that contain strings for translation. Usually your python sources
 i18nSources = pythonSources + ["buildVars.py"]
 
